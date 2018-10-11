6b1e26f29ae027cd3f6134e29133d65a325d7419
Cédric Bosdonnat <cbosdonnat@suse.com>
2018-10-11 17:00:48 +0200

    Add REST API to get virtual networks infos
    In order for the Web UI to access the list of networks, expose a REST
    API providing the data. This will be used in the VM creation UI and in
    the virtual networks management one.

--- java/code/src/com/suse/manager/webui/Router.java
+++ java/code/src/com/suse/manager/webui/Router.java
@@ -34,6 +34,7 @@ import com.suse.manager.webui.controllers.SystemsController;
 import com.suse.manager.webui.controllers.TaskoTop;
 import com.suse.manager.webui.controllers.VirtualGuestsController;
 import com.suse.manager.webui.controllers.VirtualHostManagerController;
+import com.suse.manager.webui.controllers.VirtualNetsController;
 import com.suse.manager.webui.controllers.VisualizationController;
 import com.suse.manager.webui.errors.NotFoundException;
 import java.util.HashMap;
@@ -342,6 +343,8 @@ public class Router implements SparkApplication {
                 withUser(VirtualGuestsController::getGuest));
         get("/manager/api/systems/details/virtualization/guests/:sid/domains_capabilities",
                 withUser(VirtualGuestsController::getDomainsCapabilities));
+        get("/manager/api/systems/details/virtualization/nets/:sid/data",
+                withUser(VirtualNetsController::data));
     }
 
     private void initContentManagementRoutes(JadeTemplateEngine jade) {
